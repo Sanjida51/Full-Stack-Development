d839ff29a4b4ccb0660fa348a4406c34693e823b
Sanjida <sanjidaakter.iiuc.ctg.bd@gmail.com>
2024-05-09 01:37:38 +0600

    sanjida

---
+++ PHP/Class01/index.php
@@ -0,0 +1,24 @@
+<?php
+//$first_Name=[1,2,3];
+//var_dump($first_Name);
+//$x=$y=$z=5;
+//echo $z;
+
+//$x="5";
+//var_dump($x);
+$FirstName="Jhon Doe";
+//echo "Our new member is $FirstName";
+//echo strlen($FirstName);
+//echo str_word_count($FirstName);
+//echo strpos($FirstName,"J");
+//echo strtoupper($FirstName);
+//echo strtolower($FirstName);
+//echo str_replace("D","J",$FirstName);
+//echo strrev($FirstName);
+//$F=" SS Sghjj   DFGH    jdhhhdj";
+//echo trim($F);
+$F_Name="pio";
+$L_Name="leo";
+//echo "$F_Name $L_Name";
+echo $F_Name." ".$L_Name;
+
